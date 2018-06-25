a2ca111513185a03f61c7fed9153c8452fbcf6e5
Jeremy Parker <jrobinparker@gmail.com>
2018-06-25 19:30:59 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,13 +1,8 @@
 class Student < ActiveRecord::Base 
   
  def to_s
-   full_name = []
    
-   @student = Student
-   student_name_array = @student.to_s
-   
-   return student_name_array.join (" ")
-
+  @student.to_s
    
  end
  
