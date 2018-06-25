7b9de5ddc6afb30f35f5746bb4835f6dad2085da
Jeremy Parker <jrobinparker@gmail.com>
2018-06-25 19:34:22 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,10 @@
 class Student < ActiveRecord::Base 
   
  def to_s
+  
+  @students = Student.all
    
-  @student.each do |student|
+  @students.each do |student|
     first_name = student.first_name.to_s 
     last_name = student.last_name.to_s 
     return first_name + " " + last_name
